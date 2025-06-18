bb8183bf8c6e173930d7bcbccfda9a7ac7bb6cf2
grifting-goat <137014275+grifting-goat@users.noreply.github.com>
2025-06-18 14:57:48 -0400

    init commit

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
-	
-})
+
+//% color="#4beb36"
+namespace logarithm {
+
+    /**
+     * loge
+     */
+    //% block="log base $base of $number"
+    //% weight=90
+    //% inlineInputMode=inline
+    export function logarithm(base: number, num: number): number {
+        if (base <= 0 || base == 1 || num <= 0) {
+            return NaN
+        }
+        return Math.log(num) / Math.log(base)
+    }
+
+
+}
