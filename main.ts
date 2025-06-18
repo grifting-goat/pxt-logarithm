be3653b84ad95d0b1aad8208bee0db531a8075eb
grifting-goat <137014275+grifting-goat@users.noreply.github.com>
2025-06-18 14:58:52 -0400

    fixed typo

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ namespace logarithm {
     /**
      * loge
      */
-    //% block="log base $base of $number"
+    //% block="log base $base of $num"
     //% weight=90
     //% inlineInputMode=inline
     export function logarithm(base: number, num: number): number {
